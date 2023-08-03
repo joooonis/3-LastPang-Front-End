90dfca10152a15cc905813d6237def44db4f12f3
jooooonis <xowns3213@naver.com>
2023-08-03 21:01:14 +0900

    💄 ui : 홈 화면 배치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useProfileStore } from '@/components/store/profileStore';
 import { NextPageWithLayout } from './_app';
 import useModal from '@/hooks/useModal';
 import Modal from '@/components/common/Modal/Modal';
+import Image from 'next/image';
 
 const Home: NextPageWithLayout = () => {
   const { isOpen, closeModal, openModal } = useModal({
@@ -12,13 +13,29 @@ const Home: NextPageWithLayout = () => {
   return (
     <div>
       <Modal isOpen={isOpen} onClose={closeModal} />
-      <main></main>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+        }}
+      >
+        <Image
+          fill
+          style={{
+            objectFit: 'contain',
+            transform: 'translateY(-8%)',
+          }}
+          src="/home.png"
+          alt="home"
+        />
+      </div>
     </div>
   );
 };
 
 Home.getLayout = function getLayout(page) {
-  return <Layout>{page}</Layout>;
+  return <Layout isHeader={false}>{page}</Layout>;
 };
 
 export default Home;
